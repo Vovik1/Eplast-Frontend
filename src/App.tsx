ad36c95cc412bbb315af0751f7ac28040508fc39
Yuliia <yulya.humenyuk143@gmail.com>
2020-06-01 16:11:46 +0300

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Home from "./pages/Home/Home";
 import FooterContainer from "./components/Footer/FooterContainer";
@@ -7,11 +8,15 @@ import FooterContainer from "./components/Footer/FooterContainer";
 function App() {
     return (
         <div className="App">
-          <HeaderContainer/>
-          <div className="mainContent">
-              <Home/>
-          </div>
-            <FooterContainer/>
+            <Router>
+                <HeaderContainer/>
+                <div className="mainContent">
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                    </Switch>
+                </div>
+                <FooterContainer/>
+            </Router>
         </div>
     );
 }
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Layout, Menu} from 'antd';
 import {LoginOutlined} from '@ant-design/icons';
+import {NavLink} from "react-router-dom";
 import LogoImg from "../../assets/images/ePlastLogotype.png";
 import LogoText from "../../assets/images/logo_PLAST.svg";
 import classes from "./Header.module.css";
@@ -11,15 +12,24 @@ const HeaderContainer = () => {
             <Menu mode="horizontal" className={classes.headerMenu}>
                 <Menu.Item className={classes.headerItem} key="1">
                     <div className={classes.headerLogo}>
-                        <img src={LogoImg} alt="Logo"/>
-                        <img src={LogoText} alt="Logo"/>
+                        <NavLink to="/">
+                            <img src={LogoImg} alt="Logo"/>
+                            <img src={LogoText} alt="Logo"/>
+                        </NavLink>
                     </div>
                 </Menu.Item>
             </Menu>
             <Menu mode="horizontal" className={classes.headerMenu}>
-                <Menu.Item className={classes.headerItem} key="2">Контакти</Menu.Item>
-                <Menu.Item className={classes.headerItem} key="3">Увійти
-                    <LoginOutlined className={classes.headerIcon}/></Menu.Item>
+                <Menu.Item className={classes.headerItem} key="2">
+                    <NavLink to="/contacts"
+                             className={classes.headerLink}
+                             activeClassName={classes.activeLink}>Контакти</NavLink></Menu.Item>
+                <Menu.Item className={classes.headerItem} key="3">
+                    <NavLink to="/signin"
+                             className={classes.headerLink}
+                             activeClassName={classes.activeLink}>Увійти
+                        <LoginOutlined className={classes.headerIcon}/></NavLink>
+                </Menu.Item>
             </Menu>
         </Layout.Header>
     )
