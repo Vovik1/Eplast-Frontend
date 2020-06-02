3603a9309d23cc42f24a503daaa8c526b04d18be
Yuliia <yulya.humenyuk143@gmail.com>
2020-06-02 19:23:12 +0300

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Home from "./pages/Home/Home";
-import FooterContainer from "./components/Footer/FooterContainer";
-import Contacts from "./pages/Contacts/Contacts";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HeaderContainer from './components/Header/HeaderContainer';
+import Home from './pages/Home/Home';
+import FooterContainer from './components/Footer/FooterContainer';
+import Contacts from './pages/Contacts/Contacts';
 
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <HeaderContainer/>
-                <div className="mainContent">
-                    <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/contacts" component={Contacts}/>
-                    </Switch>
-                </div>
-                <FooterContainer/>
-            </Router>
+  return (
+    <div className="App">
+      <Router>
+        <HeaderContainer />
+        <div className="mainContent">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contacts" component={Contacts} />
+          </Switch>
         </div>
-    );
+        <FooterContainer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
