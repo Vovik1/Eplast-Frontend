efb79604257c949c7d9a141906735374d257aca5
Taras Dyda <tarasdida22@gmail.com>
2020-06-02 03:17:34 +0300

    SignIn SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { store, persistor } from './store';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import "antd/dist/antd.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Switch>
+              <Route path="/signup" component={SignUp} />
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+          </Router>
+        </PersistGate>
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
