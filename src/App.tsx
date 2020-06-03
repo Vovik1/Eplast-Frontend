343db1a984599519a991ee3c38020d79749e02f4
Vovik1 <volodja.grytsiv1996@gmail.com>
2020-06-03 13:43:14 +0300

    Eplast desicions (#5)
    * Added header/footer and Home page
    
    * Added styles
    
    * Package dependencies
    
    * Table styles and Pagination Added
    
    Co-authored-by: Yuliia <yulya.humenyuk143@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { store, persistor } from './store';
 import SignUp from './pages/SignUp/SignUp';
 import SignIn from './pages/SignIn/SignIn';
-import "antd/dist/antd.css";
+import 'antd/dist/antd.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Home from './pages/Home/Home';
 import DecisionTable from './pages/DecisionTable/DecisionTable';
@@ -17,27 +17,27 @@ import ActionEvent from './pages/Actions/ActionEvent/ActionEvent';
 
 function App() {
   return (
-      <div className="App">
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Router>
-              <HeaderContainer/>
-              <div className="mainContent">
-                <Switch>
-                  <Route exact path="/" component={Home}/>
-                  <Route path="/signup" component={SignUp}/>
-                  <Route path="/signin" component={SignIn}/>
-                  <Route path="/decisions" component={DecisionTable}/>
-                  <Route path="/contacts" component={Contacts}/>
-                  <Route exact path="/actions" component={Actions} />
-                  <Route exact path="/actions/events/:id" component={ActionEvent} />  
-                </Switch>
-              </div>
-              <FooterContainer/>
-            </Router>
-          </PersistGate>
-        </Provider>
-      </div>
+    <div className="App">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <HeaderContainer />
+            <div className="mainContent">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/decisions" component={DecisionTable} />
+                <Route path="/contacts" component={Contacts} />
+                <Route exact path="/actions" component={Actions} />
+                <Route exact path="/actions/events/:id" component={ActionEvent} />
+              </Switch>
+            </div>
+            <FooterContainer />
+          </Router>
+        </PersistGate>
+      </Provider>
+    </div>
   );
 }
 
