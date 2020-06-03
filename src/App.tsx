ce759030b254575ec5d5fc136cc74cdd5e6c012b
khudnytskaya <54635860+khudnytskaya@users.noreply.github.com>
2020-06-03 12:50:42 +0300

    Feature/actions (#4)
    * Action and Event Page
    
    * Update .eslintrc.js

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import Home from './pages/Home/Home';
 import DecisionTable from './pages/DecisionTable/DecisionTable';
 import FooterContainer from './components/Footer/FooterContainer';
 import Contacts from './pages/Contacts/Contacts';
+import Actions from './pages/Actions/Actions';
+import ActionEvent from './pages/Actions/ActionEvent/ActionEvent';
 
 function App() {
   return (
@@ -27,6 +29,8 @@ function App() {
                   <Route path="/signin" component={SignIn}/>
                   <Route path="/decisions" component={DecisionTable}/>
                   <Route path="/contacts" component={Contacts}/>
+                  <Route exact path="/actions" component={Actions} />
+                  <Route exact path="/actions/events/:id" component={ActionEvent} />  
                 </Switch>
               </div>
               <FooterContainer/>
