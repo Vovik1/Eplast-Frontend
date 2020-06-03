1cc0c38aacf8076be7a7cb695f0e6ed790dae0bc
khudnytskaya <54635860+khudnytskaya@users.noreply.github.com>
2020-06-03 12:20:08 +0300

    Action and Event Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Actions from './pages/Actions/Actions'
+import ActionEvent from './pages/Actions/ActionEvent/ActionEvent';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/actions" component={Actions} />
+          <Route exact path="/actions/events/:id" component={ActionEvent} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
